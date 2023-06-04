68afb8147ecb997d13480d84313a5ad6bd31b3ee
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2023-06-04 14:19:42 +0200

    Adds an example on how to do a Download button link

--- src/pages/ArticleLoader.js
+++ src/pages/ArticleLoader.js
@@ -1,13 +1,16 @@
 import {useParams} from "react-router-dom";
 import {lazy, React, Suspense} from "react";
+import "./ArticleLoader.scss"
 
 function ArticleLoader() {
     let {section, subsection, article} = useParams();
     const SpecificArticle = lazy(() => import(`../articles/${section}/${subsection}/${article}`));
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <SpecificArticle/>
-        </Suspense>
+        <div className={"article"}>
+            <Suspense fallback={<div>Loading...</div>}>
+                <SpecificArticle/>
+            </Suspense>
+        </div>
     )
 }
 
