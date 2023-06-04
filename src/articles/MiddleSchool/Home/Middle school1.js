bfc060011da75ab11144a383e4fad7cd926303ce
Amandine Hourewitz <a.hourewitz@gmail.com>
2023-06-04 16:26:11 +0200

    footer format

--- src/articles/MiddleSchool/Home/Middle school1.js
+++ src/articles/MiddleSchool/Home/Middle school1.js
@@ -1,4 +1,18 @@
-function MiddleSchool1 () {
-    return <p> ceci est l'article 2</p>
+import affichearticle from "./cards/AFFICHE ARTICLE.jpg"
+
+function MiddleSchool1() {
+    return (<>
+        <h1> Back To School </h1>
+        <p> Hello everyone,</p>
+
+        <p>To start the school year with my students, I use this little file of logic riddles.
+            This allows them to think while having fun.</p>
+
+        <p>In general, students are divided into teams and the group with the most correct answers wins the game.</p>
+
+        <p>The file contains 9 riddles with the correction and is FREE.</p>
+        <img src={affichearticle}/>
+    </>)
 }
+
 export default MiddleSchool1
